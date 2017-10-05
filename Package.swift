be3c6caf09e7a87e518d4f22ff8799097571eee0
Janko Luin <janko@luin.se>
2017-10-05 10:35:41 +0200

    Create separate, testable framework target

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SlackSourcing",
-            dependencies: []),
+            dependencies: ["SlackSourcingCore"]),
+        .target(name: "SlackSourcingCore")
     ]
 )
